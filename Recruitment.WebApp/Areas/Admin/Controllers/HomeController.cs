c38910665439cc560f863921e13b5be813227fe7
quochieuu <quochieuu@gmail.com>
2020-11-07 17:03:38 +0700

    add list resumes view

--- Recruitment.WebApp/Areas/Admin/Controllers/HomeController.cs
+++ Recruitment.WebApp/Areas/Admin/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Recruitment.WebApp.Areas.Admin.ViewModel;
 
 namespace Recruitment.WebApp.Areas.Admin.Controllers
 {
@@ -13,11 +16,34 @@ namespace Recruitment.WebApp.Areas.Admin.Controllers
     [Authorize(Roles = "Admin,Recruitment")]
     public class HomeController : Controller
     {
+        private readonly IFileProvider fileProvider;
+        [Obsolete]
+        private IHostingEnvironment _hostingEnvironment;
+
+        [Obsolete]
+        public HomeController(IFileProvider fileProvider, IHostingEnvironment hostingEnvironment)
+        {
+            this.fileProvider = fileProvider;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         [Route("index")]
         [Route("")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("resume")]
+        public IActionResult Files()
+        {
+            var model = new FilesViewModel();
+            foreach (var item in this.fileProvider.GetDirectoryContents("resume"))
+            {
+                model.Files.Add(
+                    new FileDetails { Name = item.Name, Path = item.PhysicalPath });
+            }
+            return View(model);
         }
     }
 }
---
+++ Recruitment.WebApp/Models/FileDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruitment.WebApp.Models
+{
+    public class FileDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Direct { get; set; }
+    }
+
+    public class FilesViewModel
+    {
+        public List<FileDetails> Files { get; set; }
+            = new List<FileDetails>();
+    }
+}
